26bc4fdb88ac4289908883d047155afe08cbf865
Pavol Podstreleny <palo.podstreleny@gmail.com>
2020-09-25 18:27:42 +0100

    Add galery section

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+import Swiper, { Pagination, Autoplay } from "swiper";
+
+Swiper.use([Pagination, Autoplay]);
+
+// configure Swiper to use modules
+const swiper = new Swiper(".swiper-container", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  autoplay: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: true,
+  },
+});
+
 const openMenuButton = document.getElementById("menu-burger");
 const closeMenuButton = document.getElementById("close-menu");
 const navigation = document.getElementsByClassName("navigation")[0];
